98515a469cb57a4ead95249b35e64d853af98a2b
master-perfect <106263075+master-perfect@users.noreply.github.com>
2022-05-25 18:47:07 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+//confirm("Hello from master perfect")
+function myFunc() {
+         var x = 6;
+         var y = 9;
+         var sum = x + y; // adds x and y
+         var product = x * y; // multiplies y to x
+      
+         document.getElementById("demo").innerHTML = product;
+         document.getElementById("demo").innerHTML = sum;
+      }
+	  
+function myWord() {
+	
+	confirm("Hello users");
+}
+function write(){
+ var firstName = "John";
+      var lastName = "Doe";
+      var age = 30;
+      
+      // printing values
+      document.write(firstName + " " + lastName + "<br>");
+      document.write(age);
+	  document.getElementById("firstName").innerHTML = lastName;
+	  document.getElementById("lastName").innerHTML = firstName;
+}
